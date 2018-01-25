13aeb245d86c0a9326b38c6ce58a107e4c2db53e
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-25 01:04:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,31 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemPrice = Math.floor(Math.random() * 100);
+ var itemName = item;
+ var cartObject = { [itemName] : itemPrice };
+ cart.push(cartObject);
+ console.log(`${itemName} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+   console.log(`Your shopping cart is empty`);
+ } else if (cart.length === 1) {
+   console.log(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`);
+ } else if (cart.length === 2) {
+   console.log(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice} and ${cart[1].itemName} at $${cart[1].itemPrice}.`);
+ } else {
+   var bigCart = [];
+   for (var i = 0; i < cart.length; i++) {
+     bigCart.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
+   } 
+   if (i === cart.length) {
+     bigCart.push(`and ${cart[i].itemName} at $${cart[i].itemPrice}.`);
+   }
+   console.log(`In your cart, you have ${bigCart}.`)
+ }
 }
 
 function total() {
